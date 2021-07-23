f2f53e5e41ee281151bd57e542f82c29136c92af
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-23 18:23:38 +0600

    Word Replacement
    Word replacement exercise.

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+sentence = input('Input your sentence : ')
+print('Your sentence is: '+ sentence)
+
+word1 = input('The word you want replace: ')
+word2 = input('The word you want to set: ')
+print(sentence.replace(word1, word2))
