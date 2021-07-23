410b0fc6f09458df47ef1be34f25009b6994a287
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-23 18:24:43 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,23 @@
+name = 'Hasnat'
+
+#length of variable
+print(len(name))
+
+#printing ' in line.
+print('My name\'s is ',name)
+
+#concatenation 
+print('My name is '+name+'!')
+
+#indexing in words
+print('Nickname is '+name[0:3]+'!')
+
+#finding index in length
+print(name.index('s'))
+
+#replace a character
+print(name.replace('s','S'))
+
+#checking uppercase or not
+print('In uppercase, it is ' +name.upper())
+print('Is it in uppercase ?', name.isupper())
