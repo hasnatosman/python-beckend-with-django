53fcc26652738f1c6b1640c00a5f5fb318728d4f
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-23 18:25:00 +0600

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,43 @@
+print(100)
+
+#addition
+print(55 + 11)
+
+#subtraction
+print(100-22)
+
+#multiplication
+print(9*5)
+
+#division
+print(20/3)
+
+#floor dividion
+print(20//3)
+
+#reminder
+print(30%4)
+
+#only number
+print(abs(-100))
+
+#concatanate
+number = 150
+number1 = str(number)
+print('Number is '+number1)
+
+#max number
+print(max(4,8,3,9))
+
+#min number
+print(min(5,6,8,2))
+
+#round
+print(round(5.68))
+print(round(5.12))
+
+#binary
+print(bin(10))
+
+from math import *
+print(sqrt(9))
