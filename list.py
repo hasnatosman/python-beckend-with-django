4311aa3725c8388fd8a42685667824b803e00e3a
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-24 17:11:37 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+#list of countries
+countries = ['Bangladesh', 'Syria', 'Turkey', 'Afganistan', 'Palestine']
+
+#printing the list
+print(countries)
+
+#length of list
+print(len(countries))
+
+#print by calling index
+print(countries[3])
+
+#printing specific item 
+print(countries[2][1])
+
