77ee9f6aa70ec8db06433aa32484308f97d56430
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-25 10:18:51 -0700

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,15 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = ['A', 'x', 'hasnat']
+
+list2.append('Osman')
+
+list1.insert(1, 11)
+
+list2.remove('x')
+
+print(list1)
+print(list2)
+
+print(list2.index('hasnat'))
+
+print(list1.count('xx'))
